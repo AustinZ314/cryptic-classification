02d2de717ea7d1c073b7f18e63f22a85ea0e7da1
Austin Zhang <austinszhang1@gmail.com>
2026-03-16 19:16:13 -0400

    Added indicators and cleaned up classifier logic

--- classifier.py
+++ classifier.py
@@ -1,28 +1,53 @@
+import string
 from decrypt.scrape_parse import load_guardian_splits_disjoint
 
 # load data from the disjoint dataset (using disjoint in case later we decide to switch from zero-shot)
 _, _, (train, val, test) = load_guardian_splits_disjoint()
 
+# function to grab indicator words from their respective text files
+def load_indicators(filename):
+    with open(f"indicators/{filename}", "r") as file:
+        keywords = set()
+        for line in file:
+            keywords.add(line.strip())
+        return keywords
+
 # keywords for each type of clue from https://www.crosswordunclued.com/2008/09/dictionary.html
-# might be faster to have the values be sets instead of lists, using set intersection or similar method might be more efficient
+# found assemblage keywords at https://www.crosswordtools.com/cryptic-indicators.php
+# double definition keywords are the keywords from all the unused categories - classify as double_def if no matches found
 indicators = {
-    "anagram": ["", ],
-    "assemblage": ["", ],
-    "container": ["", ],
-    "hidden_word": ["", ],
-    "double_def": ["", ], 
+    "anagram": load_indicators("anagram.txt"),
+    "assemblage": load_indicators("assemblage.txt"),
+    "container": load_indicators("container.txt"),
+    "hidden_word": load_indicators("hidden_word.txt"),
+    "double_def": load_indicators("double_def.txt"), 
 }
 
 # classify the clue by looking for keywords
 def classify_clue(text):
 
     # normalize clue text (not sure if more should be done?)
-    text = text.lower()
+    text = text.upper().translate(str.maketrans('', '', string.punctuation))
+    words_in_clue = text.split()
+
+    tokens = set()
+    n = len(words_in_clue)
+    for i in range(n):
+        for j in range(i + 1, n + 1):
+            phrase = " ".join(words_in_clue[i:j])
+            tokens.add(phrase)
 
     for wordplay_type, keywords in indicators.items():
-        if any(word in text for word in keywords):
-            return wordplay_type
-    return "" # need some sort of catch all or placeholder, unsure
+        if wordplay_type == "double_def": # ensure that double_def is processed last
+            continue
+        else:
+            if not keywords.isdisjoint(tokens): return wordplay_type
+    
+    double_def_keywords = indicators.get("double_def")
+    if double_def_keywords.isdisjoint(tokens):
+        return "double_def"
+
+    return "unknown"
 
 # print some outputs for now, just want to see if its working
 for sample in test[:10]:
